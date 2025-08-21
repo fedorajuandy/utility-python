52a94a0e63b08531c7078de9b2202daf54cd357a
Fedora Yoshe Juandy <68008883+fedorajuandy@users.noreply.github.com>
2025-08-21 19:05:39 +0700

    feat: completed

--- file_transfer.py
+++ file_transfer.py
@@ -1,22 +1,60 @@
 """ Upload file somewhere above the clouds """
 
+import os
+import json
 import httpx
+from dotenv import load_dotenv
+
+
+load_dotenv()
+URL_UPLOAD = os.getenv("URL_UPLOAD")
+
 
 async def upload_image(file_path: str):
     """ Upload image to a server and return the response """
 
-    url = "https://cloud-photobooth.buildyourdreams.live/recieve-file-dreams"
+    if not URL_UPLOAD:
+        print("No URL found in environment variable.")
+        return json.dumps({
+            "status": 404,
+            "message": "URL not found.",
+        })
+
+    if not os.path.exists(file_path):
+        print(f"Folder {file_path} not found.")
+        return json.dumps({
+            "status": 404,
+            "message": "File not found."
+        })
 
     try:
         with open(file_path, "rb") as image_file:
-            files = {'image': (file_path, image_file, 'multipart/form-data')}
+            files = {
+                'image': (file_path, image_file, 'multipart/form-data')
+            }
+
             async with httpx.AsyncClient() as client:
-                response = await client.post(url, files=files)
+                response = await client.post(URL_UPLOAD, files=files)
 
         response.raise_for_status()
-        return response.json()
 
+        print(f"Succesfully upload file to {URL_UPLOAD} with response: {response.json()}.")
+        return json.dumps({
+            "status": 201,
+            "message": "File uploaded.",
+            "response": response.json()
+        })
     except httpx.RequestError as e:
-        return {"error": f"Request error: {e}"}
+        print(f"Request error: {e}.")
+        return json.dumps({
+            "status": 500,
+            "message": "Request error.",
+            "error": e
+        })
     except httpx.HTTPStatusError as e:
-        return {"error": f"HTTP error {e.response.status_code}: {e.response.text}"}
+        print(f"Request error: {e}.")
+        return json.dumps({
+            "status": e.response.status_code,
+            "message": "HTTP error.",
+            "error": e.response.text
+        })
